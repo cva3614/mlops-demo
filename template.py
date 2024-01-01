d7b353dfcfeedce24e489c9eca5473d3aed753bf
Shiva <shivaneupane3614@gmail.com>
2024-01-01 21:53:05 +0545

    Data ingestion, validation, transformation, model trainer, model evaluation completed

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: [%(message)s:]')
 
-project_name = "mlops-demo"
+project_name = "mlops_demo"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
